4e7cc0f91d285ff887dc6c8ee189a0be3b54ff55
JaeiKim <dobbykim0320@gmail.com>
2023-01-14 21:23:02 +0900

    조건문(2525_오답)

---
+++ BaekJoon/src/Bronze_2525.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Bronze_2525 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int c = sc.nextInt();
+
+		if ((m + c) > 60) {
+			h = h + 1;
+			m = (m + c) - 60;
+
+			if (((m + c) - 60) >= 60) {
+				h++;
+				m = m - 60;
+			}
+
+			if (h >= 23) {
+				h = 0;
+			}
+
+			System.out.println(h + " " + m);
+
+		} else {
+			m = m + c;
+			System.out.println(h + " " + m);
+		}
+	}
+
+}
