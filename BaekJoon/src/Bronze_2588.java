f798a54f8886127d67cd599bfeda499c4454e0c8
JaeiKim <dobbykim0320@gmail.com>
2023-01-14 18:07:06 +0900

    git연동 check

---
+++ BaekJoon/src/Bronze_2588.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bronze_2588 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		int num3 = num2 % 100;
+		int num4 = num3 % 10;
+		
+		System.out.println(num1 * num4);
+		System.out.println(num1 * (num3 / 10));
+		System.out.println(num1 * (num2 / 100));
+		System.out.println(num1 * num2);
+	}
+
+}
