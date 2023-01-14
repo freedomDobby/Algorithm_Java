ba964985e46f94ccc7a226c68310d3d8ea53555f
JaeiKim <dobbykim0320@gmail.com>
2023-01-15 01:29:58 +0900

    2023.10.14(알고리즘공부)

--- BaekJoon/src/Bronze_2525.java
+++ BaekJoon/src/Bronze_2525.java
@@ -6,26 +6,14 @@ public class Bronze_2525 {
 		int h = sc.nextInt();
 		int m = sc.nextInt();
 		int c = sc.nextInt();
-
-		if ((m + c) > 60) {
-			h = h + 1;
-			m = (m + c) - 60;
-
-			if (((m + c) - 60) >= 60) {
-				h++;
-				m = m - 60;
-			}
-
-			if (h >= 23) {
-				h = 0;
-			}
-
-			System.out.println(h + " " + m);
-
-		} else {
-			m = m + c;
-			System.out.println(h + " " + m);
-		}
+		
+		int min = 60 * h + m;
+		min += c;
+		
+		int hour = (min / 60) % 24;
+		int minute = min % 60;
+		
+		System.out.println(hour + " " + minute);
 	} 
    
 }
