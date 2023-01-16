d5b682d33e77725e83afa1784beb521fd080351f
JaeiKim <dobbykim0320@gmail.com>
2023-01-17 01:14:09 +0900

    3053(완성도 67%)

--- BaekJoon/BaekJoon_Array/src/Bronze_3052.java
+++ BaekJoon/BaekJoon_Array/src/Bronze_3052.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 public class Bronze_3052 {
@@ -12,10 +11,17 @@ public class Bronze_3052 {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt() % 42;
 		}
-		LinkedHashSet<> linkedHashSet =
-				new LinkedHashSet<>(Arrays.asList(arr));
-		
+		System.out.println(Arrays.toString(arr));
+
+		for (int i = 0; i < arr.length ; i++) {
+			for (int k = 1; k < 10; k++) {
+				System.out.printf("i(%d) : %d  k(%d) : %d\n",i,arr[i],k,arr[k]);
+				if (arr[i] == arr[k]) {
+					count++;
+				}
+			}
+		}
+
 		System.out.println(count);
 	}
-
 }
--- BaekJoon/src/Bronze_1110.java
+++
@@ -1,4 +0,0 @@
-
-public class Bronze_1110 {
-
-}
