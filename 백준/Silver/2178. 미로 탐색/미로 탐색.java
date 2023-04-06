dd64ace96208ba9e6d394aedf9daf93bf54c6105
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2023-04-06 15:07:44 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 12192 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,72 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static int n, m, nextX, nextY;
 	static int[][] map;
-	static int n;
-	static int m;
 	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0 }; // x방향배열-상하
-	static int[] dy = { 0, 0, -1, 1 }; // y방향배열-좌우
+	static int[] dr = { -1, 1, 0, 0 }; // 상하좌우
+	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
 		map = new int[n][m];
+		visited = new boolean[n][m];
+
 		for (int i = 0; i < n; i++) {
-			String s = br.readLine();
+			String str = br.readLine();
 			for (int j = 0; j < m; j++) {
-				map[i][j] = s.charAt(j) - '0';
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
 
-//		for (int i = 0; i < n; i++) {
+		visited[0][0] = true;
+		BFS(0, 0);
+//		//
+//		for (int i1 = 0; i1 < n; i1++) {
 //			for (int j = 0; j < m; j++) {
-//				System.out.print(map[i][j] + " ");
+//				System.out.print(map[i1][j] + " ");
 //			}
 //			System.out.println();
 //		}
+//		//
+		//System.out.printf("nextX : %d nextY : %d\n", nextX, nextY);
+		// Answer
+		System.out.println(map[n-1][m-1]);
 
-		visited = new boolean[n][m];
-		visited[0][0] = true;
-		bfs(0, 0);
-		System.out.println(map[n - 1][m - 1]);
 	}
 
-	public static void bfs(int x, int y) {
+	private static void BFS(int x, int y) {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] { x, y });
+		q.add(new int[] { x, y }); // 이 형식이 어색
 
+		// q가 빌 때까지
 		while (!q.isEmpty()) {
 			int now[] = q.poll();
 			int nowX = now[0];
-			//System.out.printf("now[0] : %d\n", now[0]);
 			int nowY = now[1];
-			//System.out.printf("now[1] : %d\n", now[1]);
 
 			for (int i = 0; i < 4; i++) {
-				int nextX = nowX + dx[i];
-				int nextY = nowY + dy[i];
+				nextX = nowX + dr[i];
+				nextY = nowY + dc[i];
 
-				if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
+				// 경계값 설정
+				if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
 					continue;
-				if (visited[nextX][nextY] || map[nextX][nextY] == 0)
+				}
+				if (visited[nextX][nextY] || map[nextX][nextY] == 0) {
 					continue;
+				}
 
 				q.add(new int[] { nextX, nextY });
 				map[nextX][nextY] = map[nowX][nowY] + 1;
 				visited[nextX][nextY] = true;
 			}
 		}
+
 	}
 }
