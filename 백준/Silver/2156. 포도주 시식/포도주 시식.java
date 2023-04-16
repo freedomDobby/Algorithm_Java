7f891e73714e0245260024e9c3590cf8edca3ab3
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2023-04-17 01:21:51 +0900

    [Silver I] Title: 포도주 시식, Time: 272 ms, Memory: 25680 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int[] wines = new int[n];
+		for (int i = 0; i < n; i++) {
+			wines[i] = sc.nextInt();
+		}
+
+		int[] dp = new int[n];
+		dp[0] = wines[0];
+
+		for (int i = 1; i < n; i++) {
+			if (i == 1) {
+				dp[1] = wines[0] + wines[1];
+				continue;
+			}
+			if (i == 2) {
+				dp[2] = Math.max(dp[1], Math.max(wines[0] + wines[2], wines[1] + wines[2]));
+				continue;
+			}
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wines[i], dp[i - 3] + wines[i - 1] + wines[i]));
+		}
+
+		System.out.println(dp[n - 1]);
+	}
+}
