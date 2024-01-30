02a93f830fe9640d68d2036bf11272ca11623bdf
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-01-30 20:42:29 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.62 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        // for(int i = 0; i < queries.length; i++){
+        //     Arrays.sort(queries[i]);
+        // }
+        for(int i = 0; i < queries.length; i++){            
+            for(int j = queries[i][0] ; j <= queries[i][1]; j++){
+                if(j % queries[i][2] == 0) arr[j]++;
+            }
+        }        
+        
+        return arr;
+    }
+}
