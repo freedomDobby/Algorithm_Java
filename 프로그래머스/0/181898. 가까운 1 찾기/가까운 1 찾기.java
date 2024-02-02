ab8e2966f821bb11c727eae99252c66e3f2b0882
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-02-02 18:10:31 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        int[] res = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i >= idx && arr[i] == 1) {
+                answer = i;
+                break;
+                }
+            else answer = -1;
+        }
+        
+        return answer;
+    }
+}
