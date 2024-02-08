a4c00d10d15894c047560e71f882af386d5a78ba
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-02-08 17:16:35 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int num1 = 0;
+        int num2 = 0;
+        
+        if(len1 == len2){
+            for(int i = 0; i < len1; i++){
+                num1 += arr1[i];
+                num2 += arr2[i];
+            }
+            if( num1 > num2) answer = 1;
+            else if( num1 < num2) answer = -1;
+            else answer = 0;
+        }else{
+            if( len1 > len2) answer =1;
+            else answer = -1;
+        }
+        
+        
+        return answer;
+    }
+}
