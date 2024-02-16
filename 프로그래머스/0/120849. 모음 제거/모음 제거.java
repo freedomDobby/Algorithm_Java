483a7489297a9ebe5c70dc2269abaf9ae67d0d3e
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-02-17 06:02:54 +0900

    [level 0] Title: 모음 제거, Time: 13.71 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        int len = my_string.length();
+        
+        for(int i = 0; i < len; i++){
+            char ch = my_string.charAt(i);
+            if(ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+                answer += ch;
+            }
+        }
+        
+        return answer;
+    }
+}
