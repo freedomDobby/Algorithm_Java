8aa27b58ff6ab113baf5aa40bba8ddd76322ff4e
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-03-14 02:26:42 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.42 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+        Arrays.sort(sides);
+        int min = sides[0];
+        int max = sides[1];
+        
+        for(int i = max-min+1 ; i <= max; i++) answer++;
+        for(int i = max+1; i < max+min ; i++) answer++;
+        
+        return answer;
+    }
+}
