1e3ee218a19dc20d9278c364d1d063cfef42160c
Jaei Kim <107250341+freedomDobby@users.noreply.github.com>
2024-03-18 23:57:42 +0900

    [level 0] Title: A로 B 만들기, Time: 0.40 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        char[] c1 = before.toCharArray();
+        char[] c2 = after.toCharArray();
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        
+        if(Arrays.equals(c1, c2)) answer = 1;
+        else answer = 0;
+        
+        return answer;
+    }
+}
